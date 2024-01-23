395b12dc0cef15fc63f5a8a79137548756a23b73
Sarah <33396785+saralavanchy@users.noreply.github.com>
2024-01-23 09:33:57 -0500

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,2 +1,5 @@
 MY_TOKEN = "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"
 call_some_api(TOKEN)
+
+MY_TOKEN = "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sadadsar23r32dfswfdasfassffa"
+call_another_api(MY_TOKEN)
